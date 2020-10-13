9ceb2f38f21d70557c575ee521e7a4bec06f0f2b
mainamuriuki274 <muriukimainalewis.com>
2020-10-13 11:25:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tensorflow_image_classification/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tensorflow_image_classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
